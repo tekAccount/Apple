7d741359c48786c2702e71019e3a03cdbb7acc7e
shaheeraghanizada <shahheraghanizada23@gmail.com>
2022-11-09 20:14:35 -0500

    hi how are

--- src/test/Github.java
+++ src/test/Github.java
@@ -1,5 +1,15 @@
 package test;
 
 public class Github {
+	public static void main(String[] args) {
+		System.out.println("How is the weather today");
+System.out.println("hihihihijih");
+	}
+
+public void test(){
+	System.out.println("hihi");
+	
+
+	}
 
 }
